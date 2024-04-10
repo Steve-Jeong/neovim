3b581f9a8ae1c2a46cf62cbffbf4e191bfa24c41
Steve Jeong <jst0930@gmail.com>
2024-04-10 08:34:38 +0300

    telescope-ui-select추가

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,31 +11,29 @@ return {
 	  "williamboman/mason-lspconfig.nvim",
     config = function()
       require('mason-lspconfig').setup({
-        ensure_installed = { "lua_ls", "tsserver", "gopls", "cssls", "dockerls", 
-          "docker_compose_language_service", "eslint", "html", "jsonls", "pylsp",
+        ensure_installed = { "lua_ls", "tsserver", "cssls", "dockerls",
+          "docker_compose_language_service", "eslint", "html", "jsonls",
           "tailwindcss"}
       })
     end
 	},
-  {
-    "neovim/nvim-lspconfig",
-    config = function()  
-      local lspconfig = require('lspconfig')
-      lspconfig.lua_ls.setup({})
-      lspconfig.tsserver.setup({})
-      lspconfig.gopls.setup({})
-      lspconfig.cssls.setup({})
-      lspconfig.dockerls.setup({})
-      lspconfig.docker_compose_language_service.setup({})
-      lspconfig.eslint.setup({})
-      lspconfig.html.setup({})
-      lspconfig.jsonls.setup({})
-      lspconfig.pylsp.setup({})
-      lspconfig.tailwindcss.setup({})
+	  {
+	    "neovim/nvim-lspconfig",
+	    config = function()
+	      local lspconfig = require('lspconfig')
+	      lspconfig.lua_ls.setup({})
+	      lspconfig.tsserver.setup({})
+	      lspconfig.cssls.setup({})
+	      lspconfig.dockerls.setup({})
+	      lspconfig.docker_compose_language_service.setup({})
+	      lspconfig.eslint.setup({})
+	      lspconfig.html.setup({})
+	      lspconfig.jsonls.setup({})
+	      lspconfig.tailwindcss.setup({})
 
-      keyMapper('K', vim.lsp.buf.hover)
-      keyMapper('gd', vim.lsp.buf.definition)
-      keyMapper('<leader>ca', vim.lsp.buf.code_action)
-    end
+	      keyMapper('K', vim.lsp.buf.hover)   -- K를 누르면 context정보를 보여줌 
+	      keyMapper('gd', vim.lsp.buf.definition)  --  gd를 누르면 정의 파일로 이동
+	      keyMapper('<leader>ca', vim.lsp.buf.code_action)  -- 오류시 <leader>ca를 누르면 code action을 보여줌.
+	    end
   }
 }
